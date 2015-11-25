e88175c36290183c9aede8fdf635b80376e5c00d
Christian Noel Reyes <darkcolonist@gmail.com>
2015-11-25 11:37:07 +0800

    implemented uniqid and auto-refresh of front-end checker queue

--- public/controllers/jobs.js
+++ public/controllers/jobs.js
@@ -4,6 +4,9 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
   var refresh = function() {
     $http.get('/jobs').success(function(response){
       $scope.jobs = response;
+
+      setTimeout(refresh, 1000);
+      // $timeout(refresh, 1000);
     })
   }
 
--- server.js
+++ server.js
@@ -1,25 +1,46 @@
 var express    = require('express'),
+    async      = require('async'),
+    uniqid     = require('uniqid'),
     app        = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    http       = require('http')
+    server     = http.createServer(app)
+    io         = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+var settings = {
+  concurrency : 5
+};
+
 var jobs = [
-  {
-    url : "http://www.example.com",
-    data : { one : "this is the number one" },
-    callback : "http://www.example.com/callback"
-  }
+  // {
+  //   url : "http://www.example.com",
+  //   data : { one : "this is the number one" },
+  //   callback : "http://www.example.com/callback"
+  // }
 ];
 
-app.post('/enqueue', function(req, res){
-  console.log("attempting to enqueue:");
-  console.log(req.body);
+job_queue = async.queue(function (task, finished) {
+  task.obj.finished = finished;
+  task.execute(task.obj);
+}, settings.concurrency);
 
+app.post('/enqueue', function(req, res){
+  if(!util.is_url(req.body.url))
+    res.json(
+      { 
+        message : "ERROR: url parameter is not a valid url"
+      }
+    );
+  
   job = {
+    uid : uniqid(),
+    queue : "main",
+    status : "waiting",
     url : req.body.url,
     data : req.body.data,
     callback : req.body.callback
@@ -29,9 +50,8 @@ app.post('/enqueue', function(req, res){
 
   res.json(
     { 
-      message : "job's done",
-      job : job,
-      req : req.body
+      message : "SUCCESS: job added",
+      job : job
     }
   );
 });
@@ -40,5 +60,17 @@ app.get('/jobs', function(req, res){
   res.json(jobs);
 });
 
-app.listen(1028);
+util = {
+  is_url : function(str){
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return pattern.test(str);
+  }
+}
+
+server.listen(1028);
 console.log("Server is listening to port 1028");
